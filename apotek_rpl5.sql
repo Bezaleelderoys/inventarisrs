59c30063a140a605e882f7e21bfe16748f0201d1
Bezaleel <142726056+Bezaleelderoys@users.noreply.github.com>
2024-08-05 09:05:12 +0800

    Add files via upload

---
+++ apotek_rpl5.sql
@@ -0,0 +1,241 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 05, 2024 at 03:04 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `apotek_rpl5`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_detail_transaksi`
+--
+
+CREATE TABLE `tb_detail_transaksi` (
+  `iddetailtransaksi` int(4) NOT NULL,
+  `idtransaksi` int(3) NOT NULL,
+  `idobat` int(4) NOT NULL,
+  `jumlah` int(3) NOT NULL,
+  `hargasatuan` double NOT NULL,
+  `totalharga` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_karyawan`
+--
+
+CREATE TABLE `tb_karyawan` (
+  `idkaryawan` int(4) NOT NULL,
+  `namakaryawan` varchar(50) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `telp` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_login`
+--
+
+CREATE TABLE `tb_login` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(65) NOT NULL,
+  `leveluser` varchar(50) NOT NULL,
+  `idkaryawan` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_obat`
+--
+
+CREATE TABLE `tb_obat` (
+  `id_obat` int(4) NOT NULL,
+  `id_supplier` int(4) NOT NULL,
+  `namaobat` varchar(100) NOT NULL,
+  `kategoriobat` varchar(50) NOT NULL,
+  `hargajual` double NOT NULL,
+  `hargabeli` double NOT NULL,
+  `stok_obat` int(11) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pelanggan`
+--
+
+CREATE TABLE `tb_pelanggan` (
+  `idpelanggan` int(4) NOT NULL,
+  `namalengkap` varchar(50) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `telp` int(15) NOT NULL,
+  `usia` int(3) NOT NULL,
+  `buktifotoresep` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_supplier`
+--
+
+CREATE TABLE `tb_supplier` (
+  `id_supplier` int(4) NOT NULL,
+  `perusahaan` varchar(100) NOT NULL,
+  `telp` varchar(20) NOT NULL,
+  `alamat` text NOT NULL,
+  `keterangan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_transaksi`
+--
+
+CREATE TABLE `tb_transaksi` (
+  `idtransaksi` int(4) NOT NULL,
+  `idpelanggan` int(4) NOT NULL,
+  `idkaryawan` int(4) NOT NULL,
+  `tgltransaksi` date NOT NULL,
+  `kategoripelanggan` varchar(20) NOT NULL,
+  `totalbayar` double NOT NULL,
+  `bayar` double NOT NULL,
+  `kembali` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_detail_transaksi`
+--
+ALTER TABLE `tb_detail_transaksi`
+  ADD PRIMARY KEY (`iddetailtransaksi`),
+  ADD KEY `FK_idobat` (`idobat`),
+  ADD KEY `FK_idtransaksi` (`idtransaksi`);
+
+--
+-- Indexes for table `tb_karyawan`
+--
+ALTER TABLE `tb_karyawan`
+  ADD PRIMARY KEY (`idkaryawan`);
+
+--
+-- Indexes for table `tb_login`
+--
+ALTER TABLE `tb_login`
+  ADD PRIMARY KEY (`username`),
+  ADD KEY `idkaryawan` (`idkaryawan`);
+
+--
+-- Indexes for table `tb_obat`
+--
+ALTER TABLE `tb_obat`
+  ADD PRIMARY KEY (`id_obat`),
+  ADD KEY `FK_supplier` (`id_supplier`);
+
+--
+-- Indexes for table `tb_pelanggan`
+--
+ALTER TABLE `tb_pelanggan`
+  ADD PRIMARY KEY (`idpelanggan`);
+
+--
+-- Indexes for table `tb_supplier`
+--
+ALTER TABLE `tb_supplier`
+  ADD PRIMARY KEY (`id_supplier`);
+
+--
+-- Indexes for table `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  ADD PRIMARY KEY (`idtransaksi`),
+  ADD KEY `FK_karyawan` (`idkaryawan`),
+  ADD KEY `FK_pelanggan` (`idpelanggan`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_detail_transaksi`
+--
+ALTER TABLE `tb_detail_transaksi`
+  MODIFY `iddetailtransaksi` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_karyawan`
+--
+ALTER TABLE `tb_karyawan`
+  MODIFY `idkaryawan` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_pelanggan`
+--
+ALTER TABLE `tb_pelanggan`
+  MODIFY `idpelanggan` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  MODIFY `idtransaksi` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_detail_transaksi`
+--
+ALTER TABLE `tb_detail_transaksi`
+  ADD CONSTRAINT `FK_idobat` FOREIGN KEY (`idobat`) REFERENCES `tb_obat` (`id_obat`),
+  ADD CONSTRAINT `FK_idtransaksi` FOREIGN KEY (`idtransaksi`) REFERENCES `tb_transaksi` (`idtransaksi`);
+
+--
+-- Constraints for table `tb_login`
+--
+ALTER TABLE `tb_login`
+  ADD CONSTRAINT `FK_login` FOREIGN KEY (`idkaryawan`) REFERENCES `tb_karyawan` (`idkaryawan`);
+
+--
+-- Constraints for table `tb_obat`
+--
+ALTER TABLE `tb_obat`
+  ADD CONSTRAINT `FK_supplier` FOREIGN KEY (`id_supplier`) REFERENCES `tb_supplier` (`id_supplier`);
+
+--
+-- Constraints for table `tb_transaksi`
+--
+ALTER TABLE `tb_transaksi`
+  ADD CONSTRAINT `FK_karyawan` FOREIGN KEY (`idkaryawan`) REFERENCES `tb_karyawan` (`idkaryawan`),
+  ADD CONSTRAINT `FK_pelanggan` FOREIGN KEY (`idpelanggan`) REFERENCES `tb_pelanggan` (`idpelanggan`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
